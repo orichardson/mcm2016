b3bc49f039481dc15d53d0380d4ee9ee5a85dffa
maz906 <mz84092@gmail.com>
2016-01-30 08:51:35 -0700

    adding code

--- data/rnn.py
+++ data/rnn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import itertools
+import random
 import numpy as np
 
 ## keep the headers, can use .iloc (integer locate)
@@ -13,7 +14,10 @@ def load_data():
 		y_files.append(df)
 
 	X_data = x_files[0].append(x_files[1])
+	X_data.sort_values('unitid', inplace=True, kind='heapsort')
 	Y_data = y_files[0].append(y_files[1:])
+	Y_data = Y_data.rename(columns = { '\xef\xbb\xbfUNITID' : 'unitid' })
+	Y_data.sort_values('unitid', inplace=True, kind='heapsort')
 	
 	return X_data, Y_data
 
@@ -21,17 +25,58 @@ def load_data():
 
 # distribution of years
 
-def get_year_distrib(data):
+def id_to_year(data):
 	id_year = data[["unitid", "academicyear"]]
 	
 	data_unitid = id_year.iloc[:, 0]
-	id_to_year = { x : [] for x in data_unitid }
+	id2year = { x : [] for x in data_unitid }
 	
 	for i, item in enumerate(data_unitid):
-		id_to_year[item].append(id_year.iloc[i, 1])
+		id2year[item].append(id_year.iloc[i, 1])
+
+	return id2year
+
+def get_year_distrib(data):
+	"""Determines how many universities we have x years of data for, for x = 1,...,28"""
+	id2year = id_to_year(data)
 
 	vals, counts = np.unique([len(x) for x in id_to_year.values()], return_counts=True)
 	return dict(zip(list(vals), list(counts)))
 
+def years_are_contiguous(data):
+	"""Determines for how many universities have contiguous year data"""
+	id2year = id_to_year(data)	
+
+	vals, counts = np.unique([is_contiguous(sorted(x)) for x in id2year.values()], return_counts=True)
+	return dict(zip(list(vals), list(counts)))
+
+def is_contiguous(alist):
+	"""Determines if a list consists of contiguous integers"""
+	return map(lambda x: x - alist[0], alist) == range(len(alist))
 
 
+def same_schools(X, Y, contiguous=True):
+	X_id2year = id_to_year(X)	
+	Y_id2year = id_to_year(Y)
+
+	if contiguous:
+		X_schools = set(x for x in X_id2year if is_contiguous(X_id2year[x]))
+		Y_schools = set(y for y in Y_id2year if is_contiguous(Y_id2year[y]))
+	else:
+		X_schools = set(x for x in X_id2year if 1 <= len(X_id2year[x]) and len(X_id2year[x]) <= 28)
+		Y_schools = set(y for y in Y_id2year if 1 <= len(Y_id2year[y]) and len(Y_id2year[y]) <= 28)		
+
+	return X_schools & Y_schools
+	
+def create_test(X, Y, percent, contiguous=True):
+	same = list(same_schools(X, Y, contiguous))
+	cutoff = int(percent * len(same))
+	random.shuffle(same)
+	restricted = set(same[:cutoff])	 
+	unrestricted = set(same[cutoff:])	
+	
+	return X[X['unitid'].isin(restricted)], Y[Y['unitid'].isin(restricted)], X[X['unitid'].isin(unrestricted)], Y[Y['unitid'].isin(unrestricted)]
+
+		
+		
+	
