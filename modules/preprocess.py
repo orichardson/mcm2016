dc0824572a829e7b9977c36bd9482c5e144ab4ab
Oliver Richardson <orichardson95@gmail.com>
2016-01-30 17:11:23 -0700

    working on model. preparing to merge with Lexie

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -42,8 +42,8 @@ def load(kind='scorecard', numeric_only=True, sort=True):
 	
 	if numeric_only:
 		df = df.select_dtypes(include=['int64', 'float64'])
-	
-     if sort:
+		
+	if sort:
          df.sort_values('unitid', inplace=True, kind='heapsort')
 
 	return df
